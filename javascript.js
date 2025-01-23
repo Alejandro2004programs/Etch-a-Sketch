02ba82fb28cc6dd332dafa3bf0caf7556facd18c
Alejandro Alvarez <ala007@ucsd.edu>
2025-01-22 19:37:38 -0800

    everything of basic functionality is working except the resize button

--- javascript.js
+++ javascript.js
@@ -1,30 +1,66 @@
 const GridContainer = document.querySelector(".container");
+let gridSize = 16;
 
-function fillGrid() {
+function fillGrid(gridSize) {
     let i, j;
-    for(i = 0; i < 16; i++) {
-        const gridSquaresFirstCol = document.createElement("div");
-        GridContainer.appendChild(gridSquaresFirstCol);
-        for(j = 0; j < 16; j++) {
+    for(i = 0; i < gridSize; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.setAttribute("class", "row");
+        GridContainer.appendChild(gridRow);
+        for(j = 0; j < gridSize; j++) {
             const gridSquares = document.createElement("div");
-            gridSquaresFirstCol.appendChild(gridSquares);
-
+            gridSquares.setAttribute("class", "square");
+            gridRow.appendChild(gridSquares);
         }
     }
     return;
-
 }
+
+function removeGrid(currentGridSize) {
+    let i, j = 0;
+    for(i = 0; i < currentGridSize ** 2 - 1; i++) {
+        const square = document.querySelector(".square");
+        square.remove();
+    }
+    for(j = 0; j < currentGridSize; j++) {
+        const row = document.querySelector(".row");
+        row.remove();
+    }
+}
+
+function CalculateSizeFromInput(input) {
+    input = Number(input);
+    if(typeof input != "number") {
+        return "ERROR";
+    }
+    input = Math.floor(input);
+    if(input > 100) {
+        return "ERROR";
+    }
+    return GridSize;
+}
+
+fillGrid(16);
 
 const newGridContainer = document.querySelectorAll(".container");
 
-fillGrid();
 
 newGridContainer.forEach((gridSquare) => {
-    gridSquare.addEventListener("mouseover", () => {
-        gridSquare.setAttribute("class", "hoverEffect");
+    gridSquare.addEventListener("mouseover", function(e) {
+        if(e.target.classList == "square") {
+            e.target.setAttribute("id", "hoverEffect");
+        }
     });
-    gridSquare.addEventListener("mouseleave", () => {
-        gridSquare.removeAttribute("class", ".hoverEffect");
+    gridSquare.addEventListener("mouseout", function(e) {
+        e.target.removeAttribute("id", "hoverEffect");
     });
 });
 
+const resetButton = document.querySelector(".top-button");
+resetButton.addEventListener("click", () => {
+    removeGrid(gridSize);
+    let newGridSize = prompt("Choose a new number of squares for columns and rows. Max size is 100: ");
+
+    fillGrid(newGridSize);
+    gridSize = newGridSize;
+});
